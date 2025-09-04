8aa8095ed5e57fe208ff643553dd6223a1b1605b
Steffan Harris <2676185+Steffan-Harris@users.noreply.github.com>
2025-09-04 15:20:27 +0100

    added breakpoints to homepage added prettier

--- src/components/TextArt.tsx
+++ src/components/TextArt.tsx
@@ -1,13 +1,16 @@
 import type { PropsWithChildren } from "react";
 import styles from "./TextArt.module.css";
+import clsx from "clsx";
 
 interface TextArtProps {
   label: string;
+  className?: string;
+  size?: 'medium' | 'large';
 }
 
-function TextArt({ label, children }: PropsWithChildren<TextArtProps>) {
+function TextArt({ label, className, size = 'medium', children }: PropsWithChildren<TextArtProps>) {
   return (
-    <pre aria-label={label} className={styles.textArt}>
+    <pre aria-label={label} className={clsx(styles.textArt, styles[size], className)}>
       {children}
     </pre>
   );
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,6 +2,8 @@ import { createFileRoute, Link } from "@tanstack/react-router";
 import styles from "./index.module.css";
 import Links from "../components/Links";
 import Copyright from "../components/Copyright";
+import mailbox from '../assets/mailbox.art'
+import TextArt from "../components/TextArt";
 
 export const Route = createFileRoute("/")({
   component: Index,
@@ -10,36 +12,45 @@ export const Route = createFileRoute("/")({
 function Index() {
   return (
     <div className={styles.homepage}>
-      <img src="logo.png" alt="Photo of Steffan" className={styles.avatar} />
-      <p className={styles.intro}>
-        I am a frontend developer and UI designer creating beautiful and
-        functional web apps focusing on the react and javascript ecosystem
-      </p>
-      <nav className={styles.navigation}>
-        <Links>
-          <li>
-            <Link to="/about">
-              <span>About me</span>
-            </Link>
-          </li>
-          <li>
-            <Link to="/projects">
-              <span>Projects</span>
-            </Link>
-          </li>
-          <li>
-            <a href="mailto:hello@steffan.lol">
-              <span>Email</span>
-            </a>
-          </li>
-          <li>
-            <a href="https://www.linkedin.com/in/steffanharris">
-              <span>LinkedIn</span>
-            </a>
-          </li>
-        </Links>
-      </nav>
-      <Copyright />
+      <div className={styles.avatar}>
+        <img src="logo.png" alt="Photo of Steffan" />
+      </div>
+      <div className={styles.content}>
+        <p className={styles.intro}>
+          I am a frontend developer and UI engineer creating beautiful and
+          functional web apps focusing on the react and javascript ecosystem
+        </p>
+        <nav className={styles.nav}>
+          <Links>
+            <li>
+              <Link to="/about">
+                <span>About me</span>
+              </Link>
+            </li>
+            <li>
+              <Link to="/projects">
+                <span>Projects</span>
+              </Link>
+            </li>
+            <li>
+              <a href="mailto:hello@steffan.lol">
+                <span>Email</span>
+              </a>
+            </li>
+            <li>
+              <a href="https://www.linkedin.com/in/steffanharris">
+                <span>LinkedIn</span>
+              </a>
+            </li>
+          </Links>
+        </nav>
+        <div className={styles.footer}>
+          <TextArt size='large' label='ASCII art depicting a computer mailbox' className={styles.mailboxArt}>
+            {mailbox}
+          </TextArt>
+          <Copyright />
+        </div>
+      </div>
     </div>
   );
 }
